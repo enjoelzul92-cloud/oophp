9305a2dbbbe8a18d91375cb45ad397bc6ecae29c
Zulkifli Sabili Rosad <enjoelzul92@gmail.com>
2026-01-31 20:45:24 +0700

    belajar static-keyword

---
+++ static-keyword.php
@@ -0,0 +1,34 @@
+<?php
+// class ContohStatic {
+//     public static $angka = 1;
+
+//     public static function hallo() {
+//         return "Hallo " . self::$angka++ . " kali.";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::hallo();
+// echo "<hr>";
+// echo ContohStatic::hallo();
+
+class Contoh {
+    public static $angka = 1;
+
+    public function halo() {
+        return "Halo " . self::$angka++ . " kali. <br>";
+    }
+}
+
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
+echo "<hr>";
+
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
