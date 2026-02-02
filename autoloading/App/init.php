d6700f8c65505000ec447263ba54e94f8d89dff0
Zulkifli Sabili Rosad <enjoelzul92@gmail.com>
2026-02-02 22:42:57 +0700

    belajar namaspace

--- autoloading/App/init.php
+++ autoloading/App/init.php
@@ -1,12 +1,17 @@
 <?php
-// require_once 'Produk/InfoProduk.php';
-// require_once 'Produk/Produk.php';
-// require_once 'Produk/Komik.php';
-// require_once 'Produk/Game.php';
-// require_once 'Produk/CetakInfoProduk.php';
-function autoload($class) {
+require_once 'Produk/InfoProduk.php';
+require_once 'Produk/Produk.php';
+require_once 'Produk/Komik.php';
+require_once 'Produk/Game.php';
+require_once 'Produk/CetakInfoProduk.php';
+require_once 'Produk/User.php';
+require_once 'Service/User.php';
 
-}
-spl_autoload_register(function($class){
-    require_once __DIR__ . '/Produk/' . $class . '.php';
-});
+
+
+// function autoload($class) {
+
+// }
+// spl_autoload_register(function($class){
+//     require_once __DIR__ . '/Produk/' . $class . '.php';
+// });
